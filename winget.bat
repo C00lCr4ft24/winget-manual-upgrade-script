b512a9f07ac96a415eade6b641a83132fe00188c
C00lCr4ft24 <68408671+C00lCr4ft24@users.noreply.github.com>
2025-02-24 02:37:20 +0100

    added include unknown and set window size of cmd

--- winget.bat
+++ winget.bat
@@ -1,7 +1,8 @@
 @echo off
+mode con: cols=90 lines=20
 set exitcode=0
 :winget
-winget upgrade
+winget upgrade --include-unknown
 echo.
 set /p user_input=Enter what you want to upgrade: 
 if %user_input%==%exitcode% (
